f136d9235e800193daf81ba653cf81f690dcf8ec
sunbo <docsun.bo@gmail.com>
2019-09-12 16:39:31 +0800

    add

--- unpackage/dist/build/app-plus/pages/collection/main.js
+++
@@ -1,2 +0,0 @@
-setCssToHead(["@charset \x22UTF-8\x22;\n.",[1],"container.",[1],"data-v-38e370a4 { background-color: #f5f5f9; min-height: 100vh; }\n.",[1],"container .",[1],"margin.",[1],"data-v-38e370a4 { margin-bottom: ",[0,20],"; }\n",],undefined,{path:"./pages/collection/main.wxss"})();
-document.dispatchEvent(new CustomEvent("generateFuncReady", { detail: { generateFunc: $gwx('./pages/collection/main.wxml') } }));
